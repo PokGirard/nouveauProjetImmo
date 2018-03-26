ee3b009f42401c274acf4a5fe815e37ae7b25294
LeaRies9 <34568513+LeaRies9@users.noreply.github.com>
2018-03-26 13:32:51 +0200

    correction contenu projet

---
+++ nouveauProjetImmo-master/TousLesCS_versionPaul/TousLesCS/EcranAccueil/PropositionVisite.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace EcranAccueil
+{
+    partial class PropositionVisite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelTitre = new System.Windows.Forms.Label();
+            this.labelCommercial = new System.Windows.Forms.Label();
+            this.labelClient = new System.Windows.Forms.Label();
+            this.labelBien = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.textBoxCommercial = new System.Windows.Forms.TextBox();
+            this.textBoxPrenomClient = new System.Windows.Forms.TextBox();
+            this.textBoxDesignation = new System.Windows.Forms.TextBox();
+            this.textBoxNomClient = new System.Windows.Forms.TextBox();
+            this.labelNomClient = new System.Windows.Forms.Label();
+            this.labelPrenomClient = new System.Windows.Forms.Label();
+            this.textBoxAdresse = new System.Windows.Forms.TextBox();
+            this.labelAdresse = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.buttonCréer = new System.Windows.Forms.Button();
+            this.buttonAnnuler = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LabelTitre
+            // 
+            this.LabelTitre.AutoSize = true;
+            this.LabelTitre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTitre.Location = new System.Drawing.Point(27, 9);
+            this.LabelTitre.Name = "LabelTitre";
+            this.LabelTitre.Size = new System.Drawing.Size(266, 13);
+            this.LabelTitre.TabIndex = 0;
+            this.LabelTitre.Text = "CREATION D\'UNE PROPOSITION DE VISITE";
+            // 
+            // labelCommercial
+            // 
+            this.labelCommercial.AutoSize = true;
+            this.labelCommercial.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCommercial.Location = new System.Drawing.Point(27, 60);
+            this.labelCommercial.Name = "labelCommercial";
+            this.labelCommercial.Size = new System.Drawing.Size(88, 13);
+            this.labelCommercial.TabIndex = 6;
+            this.labelCommercial.Text = "COMMERCIAL";
+            // 
+            // labelClient
+            // 
+            this.labelClient.AutoSize = true;
+            this.labelClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelClient.Location = new System.Drawing.Point(27, 98);
+            this.labelClient.Name = "labelClient";
+            this.labelClient.Size = new System.Drawing.Size(51, 13);
+            this.labelClient.TabIndex = 7;
+            this.labelClient.Text = "CLIENT";
+            // 
+            // labelBien
+            // 
+            this.labelBien.AutoSize = true;
+            this.labelBien.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBien.Location = new System.Drawing.Point(27, 192);
+            this.labelBien.Name = "labelBien";
+            this.labelBien.Size = new System.Drawing.Size(36, 13);
+            this.labelBien.TabIndex = 8;
+            this.labelBien.Text = "BIEN";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.Location = new System.Drawing.Point(27, 279);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(40, 13);
+            this.labelDate.TabIndex = 9;
+            this.labelDate.Text = "DATE";
+            // 
+            // textBoxCommercial
+            // 
+            this.textBoxCommercial.Location = new System.Drawing.Point(157, 57);
+            this.textBoxCommercial.Name = "textBoxCommercial";
+            this.textBoxCommercial.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCommercial.TabIndex = 10;
+            // 
+            // textBoxPrenomClient
+            // 
+            this.textBoxPrenomClient.Location = new System.Drawing.Point(157, 150);
+            this.textBoxPrenomClient.Name = "textBoxPrenomClient";
+            this.textBoxPrenomClient.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrenomClient.TabIndex = 11;
+            // 
+            // textBoxDesignation
+            // 
+            this.textBoxDesignation.Location = new System.Drawing.Point(157, 189);
+            this.textBoxDesignation.Name = "textBoxDesignation";
+            this.textBoxDesignation.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDesignation.TabIndex = 12;
+            // 
+            // textBoxNomClient
+            // 
+            this.textBoxNomClient.Location = new System.Drawing.Point(157, 120);
+            this.textBoxNomClient.Name = "textBoxNomClient";
+            this.textBoxNomClient.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNomClient.TabIndex = 13;
+            // 
+            // labelNomClient
+            // 
+            this.labelNomClient.AutoSize = true;
+            this.labelNomClient.Location = new System.Drawing.Point(30, 127);
+            this.labelNomClient.Name = "labelNomClient";
+            this.labelNomClient.Size = new System.Drawing.Size(29, 13);
+            this.labelNomClient.TabIndex = 14;
+            this.labelNomClient.Text = "Nom";
+            // 
+            // labelPrenomClient
+            // 
+            this.labelPrenomClient.AutoSize = true;
+            this.labelPrenomClient.Location = new System.Drawing.Point(27, 153);
+            this.labelPrenomClient.Name = "labelPrenomClient";
+            this.labelPrenomClient.Size = new System.Drawing.Size(43, 13);
+            this.labelPrenomClient.TabIndex = 15;
+            this.labelPrenomClient.Text = "Prénom";
+            // 
+            // textBoxAdresse
+            // 
+            this.textBoxAdresse.Location = new System.Drawing.Point(157, 226);
+            this.textBoxAdresse.Name = "textBoxAdresse";
+            this.textBoxAdresse.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAdresse.TabIndex = 16;
+            // 
+            // labelAdresse
+            // 
+            this.labelAdresse.AutoSize = true;
+            this.labelAdresse.Location = new System.Drawing.Point(27, 226);
+            this.labelAdresse.Name = "labelAdresse";
+            this.labelAdresse.Size = new System.Drawing.Size(45, 13);
+            this.labelAdresse.TabIndex = 17;
+            this.labelAdresse.Text = "Adresse";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(157, 279);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 18;
+            // 
+            // buttonCréer
+            // 
+            this.buttonCréer.Location = new System.Drawing.Point(79, 335);
+            this.buttonCréer.Name = "buttonCréer";
+            this.buttonCréer.Size = new System.Drawing.Size(75, 23);
+            this.buttonCréer.TabIndex = 19;
+            this.buttonCréer.Text = "Créer";
+            this.buttonCréer.UseVisualStyleBackColor = true;
+            this.buttonCréer.Click += new System.EventHandler(this.buttonCréer_Click);
+            // 
+            // buttonAnnuler
+            // 
+            this.buttonAnnuler.Location = new System.Drawing.Point(218, 335);
+            this.buttonAnnuler.Name = "buttonAnnuler";
+            this.buttonAnnuler.Size = new System.Drawing.Size(75, 23);
+            this.buttonAnnuler.TabIndex = 20;
+            this.buttonAnnuler.Text = "Annuler";
+            this.buttonAnnuler.UseVisualStyleBackColor = true;
+            this.buttonAnnuler.Click += new System.EventHandler(this.buttonAnnuler_Click);
+            // 
+            // PropositionVisite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 380);
+            this.Controls.Add(this.buttonAnnuler);
+            this.Controls.Add(this.buttonCréer);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.labelAdresse);
+            this.Controls.Add(this.textBoxAdresse);
+            this.Controls.Add(this.labelPrenomClient);
+            this.Controls.Add(this.labelNomClient);
+            this.Controls.Add(this.textBoxNomClient);
+            this.Controls.Add(this.textBoxDesignation);
+            this.Controls.Add(this.textBoxPrenomClient);
+            this.Controls.Add(this.textBoxCommercial);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelBien);
+            this.Controls.Add(this.labelClient);
+            this.Controls.Add(this.labelCommercial);
+            this.Controls.Add(this.LabelTitre);
+            this.Name = "PropositionVisite";
+            this.Text = "PropositionVisite";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelTitre;
+        private System.Windows.Forms.Label labelCommercial;
+        private System.Windows.Forms.Label labelClient;
+        private System.Windows.Forms.Label labelBien;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.TextBox textBoxCommercial;
+        private System.Windows.Forms.TextBox textBoxPrenomClient;
+        private System.Windows.Forms.TextBox textBoxDesignation;
+        private System.Windows.Forms.TextBox textBoxNomClient;
+        private System.Windows.Forms.Label labelNomClient;
+        private System.Windows.Forms.Label labelPrenomClient;
+        private System.Windows.Forms.TextBox textBoxAdresse;
+        private System.Windows.Forms.Label labelAdresse;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button buttonCréer;
+        private System.Windows.Forms.Button buttonAnnuler;
+    }
+}
---
+++ nouveauProjetImmo-master/TousLesCS_versionPaul/TousLesCS/EcranAccueil/PropositionVisite.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EcranAccueil
+{
+    public partial class PropositionVisite : Form
+    {
+
+
+        #region proprietes
+
+        public BIEN bien_fenetre_proposition { get; set; }
+
+        public COMMERCIAL commercial_fenetre_proposition { get; set; }
+
+        public FICHE_DE_SOUHAITS Fiche_fenetre_proposition { get; set; }
+
+
+        #endregion
+
+        public PropositionVisite()
+        {
+            InitializeComponent();
+        }
+
+        public PropositionVisite(FICHE_DE_SOUHAITS fiche_en_cours, BIEN bien_en_cours, COMMERCIAL commercial_en_cours)
+        {
+            Fiche_fenetre_proposition = fiche_en_cours;
+            bien_fenetre_proposition = bien_en_cours;
+            commercial_fenetre_proposition = commercial_en_cours;
+            InitializeComponent();
+            Initialisation_des_champs();
+
+        }
+
+        private void Initialisation_des_champs()
+        {
+            textBoxNomClient.Text = Fiche_fenetre_proposition.ACHETEUR.NOM_ACHETEUR;
+            textBoxPrenomClient.Text = Fiche_fenetre_proposition.ACHETEUR.PRENOM_ACHETEUR;
+            textBoxAdresse.Text = Fiche_fenetre_proposition.ACHETEUR.ADRESSE;
+            commercial_fenetre_proposition.NOM_COMMERCIAL = commercial_fenetre_proposition.NOM_COMMERCIAL.Replace(" ", string.Empty);
+            textBoxCommercial.Text = commercial_fenetre_proposition.NOM_COMMERCIAL;
+        }
+
+        private void buttonAnnuler_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonCréer_Click(object sender, EventArgs e)
+        {
+            PROPOSITION_VISITE proposition = new PROPOSITION_VISITE()
+            {
+                IDFICHESOUHAITS = Fiche_fenetre_proposition.IDFICHESOUHAITS,
+                IDBIEN = bien_fenetre_proposition.IDBIEN,
+                DATERDV = dateTimePicker1.Value
+            };
+
+
+
+            RechercherClient.modeleBase.PROPOSITION_VISITE.Add(proposition);
+
+            try
+            {
+                RechercherClient.modeleBase.SaveChanges();
+            }
+            catch (Exception e9)
+            {
+                MessageBox.Show("erreur");
+            }
+
+        }
+    }
+}
